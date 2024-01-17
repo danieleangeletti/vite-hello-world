31c7b7d7a55f95b9f7b56419a2a0b0ea7dbf29e2
danieleangeletti <angelettidaniele@gmail.com>
2024-01-17 15:47:10 +0000

    esercizio completato

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+// Importazione
+import AppTitle from "./components/AppTitle.vue";
+
+export default {
+  data() {},
+  // Dichiarazione dei componenti
+  components: {
+    AppTitle,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <!-- Utilizzo del componente -->
+    <AppTitle />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <h1>
+      {{ title }}
+    </h1>
+  </header>
+</template>
+
+<style scoped></style>
